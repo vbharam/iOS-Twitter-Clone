481d96fe7e8fd2b61f9496459a38f7a7b606c7fb
Vishal Bharam <vbharam@gmail.com>
2017-01-06 02:34:56 -0800

    Add detail ViewController and tableView

--- Twitter-Clone/TweetDetailViewController.swift
+++ Twitter-Clone/TweetDetailViewController.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class TweetDetailViewController: UIViewController {
-
+class TweetDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tweet: Tweet!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.setUpTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setUpTableView() {
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 140.0
     }
-    */
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TweetTableViewCell", for: indexPath) as? TweetTableViewCell {
+            
+            cell.configureCell(tweet: tweet)
+            
+            cell.timeLabel.text = tweet.time
+            
+            return cell
+            
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
 }
