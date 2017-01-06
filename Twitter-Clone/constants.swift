2b403f8e7de42492c2a67344e555985206c34c82
Vishal Bharam <vbharam@gmail.com>
2017-01-06 01:28:13 -0800

    Add dummy data to work with

---
+++ Twitter-Clone/constants.swift
@@ -0,0 +1,26 @@
+//
+//  constants.swift
+//  Twitter-Clone
+//
+//  Created by Vishal Bharam on 1/6/17.
+//  Copyright © 2017 Codecoop. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://api.twitter.com/1.1/"
+
+let USER_TIMELINE_URL = "statuses/user_timeline.json"
+
+
+typealias DownloadComplete = () -> ()
+
+let dummyTweet = Tweet(name: "Vishal", handle: "@vbharam91", time:
+    "Tue Jan 03 03:00:04 +0000 2017", body: "This is the world beyond the border. This is the world beyond the border. This is the world beyond the border", profilePic: "default_profile", replyCount:34, retweetCount: 654, favCount: 92)
+
+let dummyTweet1 = Tweet(name: "Vishal", handle: "@vbharam91", time:
+    "Tue Jan 03 03:00:04 +0000 2017", body: "This is the world beyond the border.", profilePic: "https://pbs.twimg.com/profile_images/718131815672721408/fLdxx6Mo_400x400.jpg", replyCount:42, retweetCount: 345, favCount: 87)
+
+
+// Dummy Data:
+var dict: [Tweet]  = [dummyTweet1, dummyTweet, dummyTweet1, dummyTweet, dummyTweet, dummyTweet1, dummyTweet, dummyTweet, dummyTweet1, dummyTweet, dummyTweet1, dummyTweet, dummyTweet, dummyTweet1, dummyTweet1, dummyTweet, dummyTweet1, dummyTweet, dummyTweet, dummyTweet1, dummyTweet,dummyTweet]
