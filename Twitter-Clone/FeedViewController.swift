d256d289bfc05b91fcc1cdadedaa57142dea263c
Vishal Bharam <vbharam@gmail.com>
2017-01-06 01:17:56 -0800

    Storyboard for user timeline screen

--- Twitter-Clone/FeedViewController.swift
+++ Twitter-Clone/FeedViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Twitter-Clone/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter-Clone
+//
+//  Created by Vishal Bharam on 1/6/17.
+//  Copyright © 2017 Codecoop. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var handleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    @IBOutlet weak var replyCount: UILabel!
+    @IBOutlet weak var retweetCount: UILabel!
+    @IBOutlet weak var favCount: UILabel!
+    
+}
