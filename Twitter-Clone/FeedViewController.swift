147e3f471ff816a40cd9d32d7ee7309715aeea31
Vishal Bharam <vbharam@gmail.com>
2017-01-08 21:29:05 -0800

    Implemented REST API for fetching tweets of @tim_cook

--- Twitter-Clone/FeedViewController.swift
+++ Twitter-Clone/FeedViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
 
-    var tweets = dict
+    var tweets: [Tweet] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
-        self.setUpTableView()
+        self.fetchTwitterData {
+            // self.tableView.reloadData()
+            self.setUpTableView()
+        }
     }
 
     
@@ -65,15 +69,64 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     
+    func fetchTwitterData(completed: @escaping DownloadComplete) {
+        
+        let headers = [
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "OAuth oauth_consumer_key=\"PL4HWurqv2yBzTBHLZwQLgJnF\",oauth_token=\"718131356593590272-WuKsvhyD6CGbkVOBr42tVc5fLjjbidm\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1483938345\",oauth_nonce=\"ShwRT3\",oauth_signature=\"q6kGCjJEILBM2ssZ1IIb2XgIm24%3D\""
+        ]
+        
+        // let userTimeline_url = BASE_URL + USER_TIMELINE_URL
+        let userTimetine_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=tim_cook"
+        
+        let forcastURL = URL(string: userTimetine_url)
+        
+        Alamofire.request(forcastURL!, headers: headers)
+            .responseJSON { response in
+                // print(response.request as Any)  // original URL request
+                // print(response.response as Any) // URL response
+                // print(response.data as Any)     // server data
+                // print(response.result as Any)   // result of response serialization
+                
+                if let data = response.result.value {
+                    // print("JSON: \(data)")
+                }
+                
+                if let dictArray = response.result.value as? [Dictionary<String, AnyObject>], dictArray.count > 0 {
+                    
+                    for tweetDict in dictArray {
+                        
+                        if let favCount = tweetDict["favorite_count"], let retweetCount = tweetDict["retweet_count"], let replyCount = tweetDict["retweet_count"], let message = tweetDict["text"], let time = tweetDict["created_at"], let user = tweetDict["user"] as? Dictionary<String, AnyObject>, let entities = tweetDict["entities"] as? Dictionary<String, AnyObject> {
+                            
+                            var inlineImageUrl: String = ""
+                            
+                            if let urls = entities["media"] as? [Dictionary<String, AnyObject>], urls.count > 0 {
+                                // Get the media URL if any
+                                inlineImageUrl = urls[0]["media_url_https"] as! String
+                            }
+                            
+                            
+                            let tweetData = Tweet(name: user["name"] as! String, handle: user["screen_name"] as! String, time: time as! String, body: message as! String, profilePic: user["profile_image_url_https"] as! String, inlinePic: inlineImageUrl, replyCount: replyCount as! Int, retweetCount: retweetCount as! Int, favCount: favCount as! Int)
+                            
+                            self.tweets.append(tweetData)
+                        }
+                    }
+                }
+            completed()
+            self.tableView.reloadData()
+        }
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "TweetDetailVC" {
             if let detailsVC = segue.destination as? TweetDetailViewController {
                 if let tweet = sender as! Tweet? {
+                    print("Inline - \(tweet.inlinePic)")
                     detailsVC.tweet = tweet
                 }
             }
         }
     }
-    
 }
+
